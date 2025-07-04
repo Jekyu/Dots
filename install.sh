fc98b27e400ee6e36cda85c0b88905f863a164cf
jekyu <jeoviedos@udistrital.edu.co>
2025-07-04 11:16:50 -0500

    Añadido Rofi

--- install.sh
+++ install.sh
@@ -4,16 +4,28 @@
 
 sudo apt update && sudo apt install -y polybar alacritty btop feh dmenu rofi playerctl obsidian mate-screenshot
 
+#Instalar ZED
+curl -f https://zed.dev/install.sh | sh
+
+#
+
+
 echo "Aplicaciones instaladas completamente"
 
 # ----- ENLACES SIMBOLICOS
 
 #Enlace i3
 ln -sf ~/Dots/i3 ~/.config/i3
+
 #Enlace polybar
 ln -sf ~/Dots/polybar ~/.config/polybar
-#Enlace polybar
+
+#Enlace Zed
 ln -sf ~/Dots/zed ~/.config/zed
+
+#Enlace rofi
+ln -sf ~/Dots/rofi ~/.config/rofi
+
 
 echo "Enlaces simbolicos creados"
 
